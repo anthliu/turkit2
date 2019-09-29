7170ef4215e1efcd9c9d64b3917452a4205a4c1e
Anthony Liu <anthliu@umich.edu>
2019-09-28 20:14:51 -0400

    initial files

---
+++ tests/basic_post.py
@@ -0,0 +1,14 @@
+from context import turkit2
+from turkit2.task import Task
+
+from jinja2 import Template
+from utils import get_client
+
+with open('basic_form.html') as f:
+    schema = Template(f.read())
+
+client = get_client('mykey.key')
+
+task = Task(client, schema, 'Test', '0.01', 'test test', 600, 6000)
+
+print(list(task.execute(verbosity=1)))
