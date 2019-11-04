2816a4d81f37068f725d04b4cb57b4ed86c05435
Anthony Liu <anthliu@umich.edu>
2019-11-04 17:13:44 -0500

    make external posting script better

--- tests/external.py
+++ tests/external.py
@@ -5,14 +5,26 @@ from context import turkit2
 from turkit2.common import External
 from utils import get_client
 
-client = get_client()
+client = get_client(None)
 
-task = External(client, 'Test2', '0.01', 'test test', 600, 6000)
+task = External(
+    client,
+    title='Test2',
+    reward='0.01',
+    description='test test',
+    duration=600,
+    lifetime=60*60*24
+)
 
-#print(task.preview(text='This library is the best!', question="Does this text follow pattern: 'enthusiastic'"))
 async def proc():
     async for answer, assignment in task.ask_async(verbosity=100, url=sys.argv[1]):
         print(answer)
         print(assignment)
 
-asyncio.run(proc())
+async def main():
+    tasks = []
+    for _ in range(20):
+        tasks.append(asyncio.create_task(proc()))
+    await asyncio.gather(*tasks)
+
+asyncio.run(main())
