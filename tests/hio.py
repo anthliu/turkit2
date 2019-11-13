07c684f5df6c2dc706de2f379e2c6568ea623e44
Anthony Liu <anthliu@umich.edu>
2019-11-12 23:45:34 -0500

    add image test to hio

--- tests/hio.py
+++ tests/hio.py
@@ -2,8 +2,11 @@ import asyncio
 
 from context import turkit2
 from turkit2.common import HumanIO
-from turkit2.primitive import IText, OText
-from utils import get_client
+from turkit2.primitive import IText, OText, IImage
+from utils import get_client, get_s3
+
+s3 = get_s3(profile='anthony')
+im = IImage(s3, 'turkit-testing')
 
 client = get_client()
 
@@ -12,6 +15,8 @@ elements = [
     ('Answer', OText('answer1')),
     ('Prompt2', IText()),
     ('Answer2', OText('answer2')),
+    ('Prompt3', im),
+    ('Answer2', OText('answer3')),
 ]
 
 task = HumanIO(client, elements, 'Test2', '0.01', 'test test', 600, 6000)
@@ -20,6 +25,7 @@ async def proc():
     parameters = {
         'Prompt': {'text': 'How are you?'},
         'Prompt2': {'text': 'Flip a coin.'},
+        'Prompt3': {'path': 'imgs/dog2.jpg'},
     } 
     async for answer, assignment in task.ask_async(verbosity=100, parameters=parameters):
         print(answer)
